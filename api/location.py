2bac379cbb78f7260626cceb6c759c3426521dce
George Tay <george_tay@u.nus.edu>
2024-10-13 21:36:38 +0800

    Guard against empty list or map

--- api/location.py
+++ api/location.py
@@ -7,7 +7,7 @@ values = dotenv_values(".env")
 def reverse_lookup_location(latitude, longitude):
     r = requests.get(f"http://api.openweathermap.org/geo/1.0/reverse?lat={latitude}&lon={longitude}&limit=1&appid={values.get("OPENWEATHER_API_KEY")}").json()
 
-    if r:
+    try:
         return r[0]["name"]
-
-    return None
+    except:
+        return None
